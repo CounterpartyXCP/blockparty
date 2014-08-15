cc5a9131d522a531fe7af22c9f768f2bb649ab68
Tyler Eaves <tylere@counterparty.co>
2014-08-15 14:17:49 -0400

    Import counterwallet build system

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function (grunt) {
+
+    var buildDir = 'build/';
+
+    var config = {
+        pkg: grunt.file.readJSON('package.json'),
+
+        build: {
+            options: {
+                buildDir: buildDir,
+                srcDir: 'src/',
+                assetsHome: 'assets/',
+                checkDeps: true,
+                depsDir: 'vendors/',
+
+            },
+            process: {
+                files: [
+                    {cwd: 'src/', src: 'index.html', dest: buildDir, expand: true},
+                    {cwd: 'src/pages/', src: '*.html', dest: buildDir+'pages/', expand: true}
+                ]
+            },
+            copy: {
+                files: [
+                    {src: 'src/robots.txt', dest: buildDir+'robots.txt'},
+                    {cwd: 'src/assets/', src: '*', dest: buildDir+'assets/', expand: true}
+                ]
+            }
+        }
+    }
+    
+    grunt.initConfig(config);
+
+    grunt.loadTasks('grunt-tasks');
+
+    grunt.registerTask('default', ['build']);
+};
