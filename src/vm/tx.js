899e673ca51a71ec122d01552e4d08aeeeedce73
Tyler Eaves <tylere@counterparty.co>
2014-09-09 16:21:54 -0400

    more tx work

--- src/vm/tx.js
+++ src/vm/tx.js
@@ -1,19 +1,15 @@
-/**
- * Created by Tyler on 8/22/2014.
- */
 define('tx',['jquery','knockout','util', 'api'], function($,ko, util, api){
-
   return function() {
-    this.tx = util.load_msgs(500);
-    this.tx_data = ko.computed(function() {
-      return _.map(_.filter(this.tx(), function(x){return x['_command'] == 'insert'}), util.parse_msg);
-    });
-    this.tx_json = ko.computed(function () {
-      console.log("tx_json computed");
-      var xs = _.map(this.tx(), JSON.stringify);
-      return xs;
 
-    });
+    this.vm = {
+     tx_hash: ko.observable()
+    }
+
+    this.load = function(callback, page){
+      console.log(page);
+
+      callback(this.vm);
+
+    }
   }
-
 });
---
+++ src/vm/txns.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Tyler on 8/22/2014.
+ */
+define('txns',['jquery','knockout','util', 'api'], function($,ko, util, api){
+
+  return function() {
+    this.tx = util.load_msgs(500);
+    this.tx_data = ko.computed(function() {
+      return _.map(_.filter(this.tx(), function(x){return x['_command'] == 'insert'}), util.parse_msg);
+    });
+    this.tx_json = ko.computed(function () {
+      console.log("tx_json computed");
+      var xs = _.map(this.tx(), JSON.stringify);
+      return xs;
+
+    });
+  }
+
+});
